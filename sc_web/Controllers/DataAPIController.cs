1158ce81e2e5b065a9bb3b3d6412c7956c289137
Alex Dillon <alexdillon@knology.net>
2019-02-02 20:26:25 -0600

    Add DataApiController for posting sensor data

---
+++ sc_web/Controllers/DataAPIController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using sc_web.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace sc_web.Controllers
+{
+    [RoutePrefix("api/Data")]
+    public class DataAPIController : ApiController
+    {
+        /// <summary>
+        /// Application DB context
+        /// </summary>
+        protected ApplicationDbContext ApplicationDbContext { get; set; }
+
+        /// <summary>
+        /// User manager - attached to application DB context
+        /// </summary>
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
+        public DataAPIController()
+        {
+            this.ApplicationDbContext = new ApplicationDbContext();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.ApplicationDbContext));
+        }
+
+        [Route("PostHeartRateData")]
+        public IHttpActionResult PostHeartRateData(Models.Chair.HeartRateSensorModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            if (!Request.Headers.TryGetValues("X-AuthKey", out var authKeyList))
+            {
+                return BadRequest("No AuthKey");
+            }
+
+            var authKey = authKeyList.FirstOrDefault();
+
+            var chair = ApplicationDbContext.Users.SelectMany(u => u.PairedChairs)
+                .Where(c => c.AuthKey == authKey)
+                .FirstOrDefault();
+                
+            if (chair == null)
+            {
+                return BadRequest("Bad AuthKey");
+            }
+
+            chair.HeartSensorData.Add(data);
+            ApplicationDbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [Route("PostPostureData")]
+        public IHttpActionResult PostPostureData(Models.Chair.PostureSensorModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            if (!Request.Headers.TryGetValues("X-AuthKey", out var authKeyList))
+            {
+                return BadRequest("No AuthKey");
+            }
+
+            var authKey = authKeyList.FirstOrDefault();
+
+            var chair = ApplicationDbContext.Users.SelectMany(u => u.PairedChairs)
+                .Where(c => c.AuthKey == authKey)
+                .FirstOrDefault();
+
+            if (chair == null)
+            {
+                return BadRequest("Bad AuthKey");
+            }
+
+            chair.PostureSensorData.Add(data);
+            ApplicationDbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [Route("PostMotionData")]
+        public IHttpActionResult PostMotionData(Models.Chair.MotionEventModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            if (!Request.Headers.TryGetValues("X-AuthKey", out var authKeyList))
+            {
+                return BadRequest("No AuthKey");
+            }
+
+            var authKey = authKeyList.FirstOrDefault();
+
+            var chair = ApplicationDbContext.Users.SelectMany(u => u.PairedChairs)
+                .Where(c => c.AuthKey == authKey)
+                .FirstOrDefault();
+
+            if (chair == null)
+            {
+                return BadRequest("Bad AuthKey");
+            }
+
+            chair.MotionSensorData.Add(data);
+            ApplicationDbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [Route("PostOccupancyData")]
+        public IHttpActionResult PostOccupancyData(Models.Chair.OccupancySessionModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            if (!Request.Headers.TryGetValues("X-AuthKey", out var authKeyList))
+            {
+                return BadRequest("No AuthKey");
+            }
+
+            var authKey = authKeyList.FirstOrDefault();
+
+            var chair = ApplicationDbContext.Users.SelectMany(u => u.PairedChairs)
+                .Where(c => c.AuthKey == authKey)
+                .FirstOrDefault();
+
+            if (chair == null)
+            {
+                return BadRequest("Bad AuthKey");
+            }
+
+            chair.OccupancySessionData.Add(data);
+            ApplicationDbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
