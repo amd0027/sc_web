aa4a1d9b1750f2aaf4ddea04e94cc396b4b072df
Alex Dillon <alexdillon@knology.net>
2019-02-03 00:44:58 -0600

    Add popper to bundleconfig for bootstrap (used for dropdown menus and more)

--- sc_web/App_Start/BundleConfig.cs
+++ sc_web/App_Start/BundleConfig.cs
@@ -23,7 +23,8 @@ namespace sc_web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                    "~/Scripts/umd/popper.js",
+                    "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
